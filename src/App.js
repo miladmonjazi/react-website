e2b6ce6e0a9d8c1be3adf50dbb5975040ca98afd
Milad Monjazi <milad.monjazi@gmail.com>
2020-09-15 17:28:41 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
-     <Router>
-       <Navbar />
-       <Switch>
-         <Route path='/' exact />
-       </Switch>
-     </Router>
-   </>
+    <>
+      <Router basename={'/react-website'}>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
